7a28fa8e592dfd8db326f47e9c17ca458ee4713b
NNinja1255 <jack82702@gmail.com>
2026-01-08 17:36:21 -0500

    Update sh_specialweapons.lua

--- gamemodes/nzombies/gamemode/special_weapons/sh_specialweapons.lua
+++ gamemodes/nzombies/gamemode/special_weapons/sh_specialweapons.lua
@@ -1,10 +1,13 @@
 
 local function RegisterDefaultSpecialWeps()
+	nzSpecialWeapons:AddKnife( "nz_quickknife_crowbar", false, 0.65 )
 	nzSpecialWeapons:AddKnife( "nz_knife", false, 0.65 )
 	nzSpecialWeapons:AddKnife( "nz_bowie_knife", true, 0.65, 2.5 )
 	nzSpecialWeapons:AddKnife( "nz_one_inch_punch", true, 0.75, 1.5 )
 
 	nzSpecialWeapons:AddGrenade( "nz_grenade", 4, false, 0.65, false, 0.4 ) -- ALWAYS pass false instead of nil or it'll assume default value
+	nzSpecialWeapons:AddGrenade( "nz_grenade_nade", 4, false, 0.65, false, 0.4 )
+	nzSpecialWeapons:AddGrenade( "nz_semtex", 4, false, 0.65, false, 0.4 )
 	nzSpecialWeapons:AddSpecialGrenade( "nz_monkey_bomb", 3, false, 3, false, 0.4 )
 	
 	nzSpecialWeapons:AddDisplay( "nz_revive_morphine", false, function(wep)
